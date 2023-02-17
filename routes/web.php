dd2e5858a7a8f603637d63b57ce94adb7ec39bd8
yogapriya024 <yogapriya@isportz.co>
2023-02-17 12:46:01 +0530

    Define Routing

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//------------------------------------------------------------Routing------------------------------------------------------------
 Route::get('/', function () {
-    return view('welcome');
-});
+    return 'Home page';
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return 'Contact';
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return 'Blog post' . $id;
+})->name('posts.show');
+
+Route::get('/recent-posts/{days_ago?}', function ($days_ago = 20) {
+    return 'Post from' . $days_ago. 'days ago';
+})->name('posts.recent.index');
+
+//`````````````````````````where condition used in RouteServiceProvider````````````````````````````````````````````````````````````````
+//Route::get('/posts/{id}', function ($id) {
+//    return 'Blog post' . $id;
+//})->where([
+//    'id' => '[0-9]+'
+//])->name('posts.show');
+//------------------------------------------------------------End Routing------------------------------------------------------------
+
